21d83663e90fd6bfa30860ff6a8b027eb59095f3
Felix Edna Santosa <felixsantosa123@gmail.com>
2025-07-12 18:38:10 +0700

    basic js wpu

--- 8_object/intro/script.js
+++ 8_object/intro/script.js
@@ -36,16 +36,49 @@
 // IPKumulatif(mahasiswa[3]);
 
 // disimpan dalam object
-var mahasiswa = {
-    nama : 'Felix Edna Santosa',
-    lulus : false,
-    IpSemester : [3.82,3.78,3.91,4],
-    IPKumulatif : function() {
-        var total = 0;
-        var ips = this.IpSemester;
-        for(var i = 0; i < ips.length; i++) {
-            total += ips[i];
-        }
-        return total;
+// var mahasiswa = {
+//     nama : 'Felix Edna Santosa',
+//     lulus : false,
+//     IpSemester : [3.82,3.78,3.91,4],
+//     IPKumulatif : function() {
+//         var total = 0;
+//         var ips = this.IpSemester;
+//         for(var i = 0; i < ips.length; i++) {
+//             total += ips[i];
+//         }
+//         return total;
+//     }
+// }
+
+// mahasiswa.IPKumulatif();
+
+// anatomi object
+// variabel di dalam object disebut properti dan function di dalam object disebut method
+// var Orang = {
+//     nama : 'Felix Santosa',
+//     umur : 20,
+//     pekerjaan : 'mahasiswa',
+//     sapa : function() {
+//         return 'Hi, nama saya' + this.nama + ' umur saya' + this.umur + ' saat ini saya sebagai' + this.pekerjaan;
+//     }
+// }
+
+// Orang.sapa();
+
+// membuat object
+var mhs = {
+    nama : 'Felix Edna',
+    umur : 20,
+    ips : [3.78, 3.82, 3.91, 4.00],
+    status : 'mahasiswa',
+    alamat : {
+        jalan : 'jl. udang windu',
+        kota : 'Jember',
+        provinsi : 'Jawa Timur'
+    },
+    sapa : function() {
+        return 'Hi, nama saya ' + this.nama + ' umur saya ' + this.umur + ' saat ini saya sebagai ' + this.status;
     }
-}
+};
+
+// tadi kita membuat object literal, untuk selanjutnya ada membuat object dengan constructor
